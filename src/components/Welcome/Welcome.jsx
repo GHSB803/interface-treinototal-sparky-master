87cd1486295e43c76f941f06bdd4da933ac212bd
Gabriel Henrique <GabrielHenri9427@gmail.com>
2024-07-30 17:18:38 -0300

    atualização 1.4

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -23,28 +23,41 @@ function Welcome() {
         );
 
     return (
-        <div className="welcome">
-            <Carousel showThumbs={false}
-                autoPlay={true}
-                infiniteLoop={true}
-                showStatus={false}
-                renderArrowPrev={renderArrowPrev}
-                renderArrowNext={renderArrowNext}
-            >
-                <div>
-                    <img src={gym1} alt="Imagem 1" className='carousel' />
-                    <h1 className='legend'>imagem 1</h1>
+        <>
+            <div className="welcome">
+                <Carousel showThumbs={false}
+                    autoPlay={true}
+                    infiniteLoop={true}
+                    showStatus={false}
+                    renderArrowPrev={renderArrowPrev}
+                    renderArrowNext={renderArrowNext}
+                >
+                    <div>
+                        <img src={gym1} alt="Imagem 1" className='carousel' />
+                        <h1 className='legend'>imagem 1</h1>
+                    </div>
+                    <div>
+                        <img src={gym2} alt="Imagem 2" className='carousel' />
+                        <p className="legend">Legenda para a Imagem 2</p>
+                    </div>
+                    <div>
+                        <img src={gym3} alt="Imagem 3" className='carousel' />
+                        <p className="legend">Legenda para a Imagem 3</p>
+                    </div>
+                </Carousel>
+            </div>
+
+            <div className='introduction'>
+                <div class="card">
+                <img src={gym2} alt="Imagem 2" className='carousel' />
+                    <div class="card-content">
+                        <h2 class="card-title">Título do Card</h2>
+                        <p class="card-text">Este é um exemplo de card usando HTML e CSS. Você pode adicionar qualquer conteúdo que desejar aqui.</p>
+                        <a href="#" class="card-button">Clique Aqui</a>
+                    </div>
                 </div>
-                <div>
-                    <img src={gym2} alt="Imagem 2" className='carousel' />
-                    <p className="legend">Legenda para a Imagem 2</p>
-                </div>
-                <div>
-                    <img src={gym3} alt="Imagem 3" className='carousel' />
-                    <p className="legend">Legenda para a Imagem 3</p>
-                </div>
-            </Carousel>
-        </div>
+            </div>
+        </>
     );
 }
 
